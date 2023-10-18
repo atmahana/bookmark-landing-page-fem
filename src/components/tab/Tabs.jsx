bc9fbc287609a280e868e05faa5f4f4a995c9152
Adham <zubairadham.ar@gmail.com>
2023-10-18 11:08:52 +0800

    style: refactor responsive styles

--- src/components/tab/Tabs.jsx
+++ src/components/tab/Tabs.jsx
@@ -12,14 +12,14 @@ const Tabs = ({ tabs, className }) => {
     <div className="w-full mx-auto">
       <div className="flex flex-col md:flex-row sm:gap-12 lg:gap-0 justify-center text-center">
         {tabs.map((tab, index) => (
-          <div key={index} className="border-t lg:border-none">
+          <div key={index} className="border-t sm:border-none">
             <button
               key={index}
               onClick={() => handleTabClick(index)}
               className={cn(
-                `px-5 py-4 lg:px-0 lg:py-6 text-[17px] lg:w-60 cursor-pointer hover:text-accent ${
+                `relative px-5 py-4 lg:px-0 lg:py-6 text-[17px] lg:w-60 cursor-pointer hover:text-accent ${
                   index === activeTab
-                    ? "after:content-[''] after:block after:w-[68%] lg:after:w-full after:h-1 relative -z-10 after:absolute after:-bottom-[1px] after:left-1/2 after:-translate-x-1/2 after:bg-accent border-accent"
+                    ? "after:content-[''] after:block after:w-[68%] lg:after:w-full after:h-1 after:absolute after:-bottom-[1px] after:left-1/2 after:-translate-x-1/2 after:bg-accent border-accent"
                     : "bg-none border-none text-secondary"
                 }`,
                 className
